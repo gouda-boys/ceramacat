41d5a6e75b604e57641a52446d948a672424955a
Isaiah Mann <isaiahbmann@gmail.com>
2019-11-08 21:53:44 -0800

    Add basic movement + jump

---
+++ Assets/Scripts/World/EnvironmentController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+    [SerializeField]
+    float scrollSpeed = 1;
+
+    [SerializeField]
+    float canvasWidth = 14;
+
+    PlatformBehavior[] platforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platforms = GetComponentsInChildren<PlatformBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        foreach(PlatformBehavior plat in platforms)
+        {
+            plat.transform.position += Vector3.left * scrollSpeed;
+        }
+    }
+
+    public void FlagForRespawn(PlatformBehavior platform)
+    {
+        platform.transform.position += Vector3.right * canvasWidth;
+    }
+}
