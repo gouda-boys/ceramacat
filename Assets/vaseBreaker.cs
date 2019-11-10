f8214ba5a48304d6fe4104367dd58e1a960218a6
emerz210 <emerz210@gmail.com>
2019-11-10 11:05:00 -0800

    walls and tail

---
+++ Assets/vaseBreaker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vaseBreaker : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 10)
+        {
+            audioSource.Play();
+        }
+        //break vase
+        Debug.Log("VASE BROKEN");
+    }
+
+}
